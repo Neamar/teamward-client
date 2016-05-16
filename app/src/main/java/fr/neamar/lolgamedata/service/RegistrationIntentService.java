d604c9898fd6ad9b824840515bf802371412aa13
Neamar <neamar@neamar.fr>
2016-05-16 19:54:43 +0100

    GCM boilerplate

---
+++ app/src/main/java/fr/neamar/lolgamedata/service/RegistrationIntentService.java
@@ -0,0 +1,55 @@
+package fr.neamar.lolgamedata.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
+
+import fr.neamar.lolgamedata.R;
+
+/**
+ * Created by neamar on 16/05/16.
+ */
+public class RegistrationIntentService extends IntentService {
+    private static final String SENT_TOKEN_TO_SERVER = "SENT_TOKEN_TO_SERVER";
+
+    private static final String TAG = "RegIntentService";
+
+    public RegistrationIntentService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        try {
+            // [START register_for_gcm]
+            // Initially this call goes out to the network to retrieve the token, subsequent calls
+            // are local.
+            // R.string.gcm_defaultSenderId (the Sender ID) is typically derived from google-services.json.
+            // See https://developers.google.com/cloud-messaging/android/start for details on this file.
+            // [START get_token]
+            InstanceID instanceID = InstanceID.getInstance(this);
+            String token = instanceID.getToken(getString(R.string.gcm_defaultSenderId),
+                    GoogleCloudMessaging.INSTANCE_ID_SCOPE, null);
+            // [END get_token]
+            Log.i(TAG, "GCM Registration Token: " + token);
+
+            // You should store a boolean that indicates whether the generated token has been
+            // sent to your server. If the boolean is false, send the token to your server,
+            // otherwise your server should have already received the token.
+            sharedPreferences.edit().putString(SENT_TOKEN_TO_SERVER, token).apply();
+            // [END register_for_gcm]
+        } catch (Exception e) {
+            Log.e(TAG, "Failed to complete token refresh", e);
+            // If an exception happens while fetching the new token or updating our registration data
+            // on a third-party server, this ensures that we'll attempt the update at a later time.
+            sharedPreferences.edit().remove(SENT_TOKEN_TO_SERVER).apply();
+        }
+    }
+}
