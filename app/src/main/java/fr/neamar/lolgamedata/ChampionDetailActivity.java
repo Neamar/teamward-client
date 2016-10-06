c816219fec8d9741500eca2a55f8aac0ba0b1e1e
Neamar <neamar@neamar.fr>
2016-10-06 20:18:06 +0100

    First work on detail view using Palette

---
+++ app/src/main/java/fr/neamar/lolgamedata/ChampionDetailActivity.java
@@ -0,0 +1,65 @@
+package fr.neamar.lolgamedata;
+
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+public class ChampionDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_champion_detail);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        final ImageView splashArtImage = (ImageView) findViewById(R.id.splashArt);
+        ImageLoader.getInstance().loadImage("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Kled_0.jpg", new SimpleImageLoadingListener() {
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                splashArtImage.setImageBitmap(loadedImage);
+                Palette.from(loadedImage).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        Palette.Swatch swatch = palette.getVibrantSwatch();
+                        Palette.Swatch darkSwatch = palette.getDarkVibrantSwatch();
+                        if (swatch != null && darkSwatch != null) {
+                            collapsingToolbarLayout.setContentScrimColor(swatch.getRgb());
+                            toolbar.setTitleTextColor(swatch.getTitleTextColor());
+
+                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                                int notificationColor = palette.getDarkMutedColor(getResources().getColor(R.color.colorPrimaryDark));
+                                Window window = getWindow();
+                                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                                window.setStatusBarColor(darkSwatch.getRgb());
+                            }
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
