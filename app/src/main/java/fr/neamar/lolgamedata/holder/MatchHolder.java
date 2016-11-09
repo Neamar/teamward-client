33e50af87b16f6b115e27afaff1a691398a4e6c7
Neamar <neamar@neamar.fr>
2016-11-09 11:48:31 +0000

    Add a victory line color

--- app/src/main/java/fr/neamar/lolgamedata/holder/MatchHolder.java
+++ app/src/main/java/fr/neamar/lolgamedata/holder/MatchHolder.java
@@ -1,6 +1,7 @@
 package fr.neamar.lolgamedata.holder;
 
 import android.support.v7.widget.RecyclerView;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ import fr.neamar.lolgamedata.pojo.Match;
 public class MatchHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private final TextView kdaText;
     private final ImageView wardImage;
+    private final View winOrLossView;
+
     private final ArrayList<ImageView> itemImages = new ArrayList<>(6);
 
     private Match match;
@@ -24,6 +27,7 @@ public class MatchHolder extends RecyclerView.ViewHolder implements View.OnClick
 
         kdaText = (TextView) view.findViewById(R.id.kda);
         wardImage = (ImageView) view.findViewById(R.id.itemWard);
+        winOrLossView = view.findViewById(R.id.winOrLossBanner);
 
         itemImages.add((ImageView) view.findViewById(R.id.item1));
         itemImages.add((ImageView) view.findViewById(R.id.item2));
@@ -39,7 +43,7 @@ public class MatchHolder extends RecyclerView.ViewHolder implements View.OnClick
     public void bind(Match match) {
         this.match = match;
 
-        kdaText.setText(String.format(kdaText.getContext().getString(R.string.kda_template), match.k, match.d, match.a));
+        kdaText.setText(Html.fromHtml(String.format(kdaText.getContext().getString(R.string.kda_template), match.k, match.d, match.a)));
 
         ImageLoader.getInstance().displayImage(match.ward, wardImage);
 
@@ -47,9 +51,11 @@ public class MatchHolder extends RecyclerView.ViewHolder implements View.OnClick
             ImageLoader.getInstance().displayImage(match.items.get(i), itemImages.get(i));
         }
 
-        for(int i = match.items.size(); i < itemImages.size(); i++) {
+        for (int i = match.items.size(); i < itemImages.size(); i++) {
             itemImages.get(i).setImageResource(R.drawable.item_emtpy);
         }
+
+        winOrLossView.setBackgroundColor(winOrLossView.getContext().getResources().getColor(match.victory ? R.color.colorGoodMatchup : R.color.colorBadMatchup));
     }
 
     @Override
