9779b1c157770cb805b0e2da47ee459c946814a0
Matthieu Bacconnier <neamar@neamar.fr>
2017-07-18 22:00:56 +0100

    Display both teams stats

--- app/src/main/java/fr/neamar/lolgamedata/tips/holder/WinrateByTimeTipHolder.java
+++ app/src/main/java/fr/neamar/lolgamedata/tips/holder/WinrateByTimeTipHolder.java
@@ -1,9 +1,11 @@
 package fr.neamar.lolgamedata.tips.holder;
 
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -12,9 +14,7 @@ import fr.neamar.lolgamedata.R;
 import fr.neamar.lolgamedata.holder.TipHolder;
 import fr.neamar.lolgamedata.tips.Tip;
 
-/**
- * Created by neamar on 7/18/2017.
- */
+import static fr.neamar.lolgamedata.R.id.graph;
 
 public class WinrateByTimeTipHolder extends TipHolder {
     private final GraphView graphView;
@@ -22,7 +22,19 @@ public class WinrateByTimeTipHolder extends TipHolder {
     private WinrateByTimeTipHolder(View itemView) {
         super(itemView);
 
-        graphView = (GraphView) itemView.findViewById(R.id.graph);
+        graphView = (GraphView) itemView.findViewById(graph);
+
+        graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
+            @Override
+            public String formatLabel(double value, boolean isValueX) {
+                if (isValueX) {
+                    return super.formatLabel(value, isValueX) + "'";
+                } else {
+                    // show currency for y values
+                    return super.formatLabel(value, isValueX) + "%";
+                }
+            }
+        });
     }
 
     public static TipHolder onCreateViewHolder(ViewGroup parent) {
@@ -36,13 +48,26 @@ public class WinrateByTimeTipHolder extends TipHolder {
     public void bind(Tip tip) {
         // WinrateByTimeTip winrateByTimeTip = (WinrateByTimeTip) tip;
 
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+        LineGraphSeries<DataPoint> blueSeries = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(20, 50),
+                new DataPoint(25, 55),
+                new DataPoint(30, 57),
+                new DataPoint(35, 52),
+                new DataPoint(40, 48),
+                new DataPoint(45, 40),
         });
-        graphView.addSeries(series);
+        blueSeries.setColor(Color.BLUE);
+        graphView.addSeries(blueSeries);
+
+        LineGraphSeries<DataPoint> redSeries = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(20, 45),
+                new DataPoint(25, 45),
+                new DataPoint(30, 47),
+                new DataPoint(35, 49),
+                new DataPoint(40, 52),
+                new DataPoint(45, 55),
+        });
+        redSeries.setColor(Color.RED);
+        graphView.addSeries(redSeries);
     }
 }
