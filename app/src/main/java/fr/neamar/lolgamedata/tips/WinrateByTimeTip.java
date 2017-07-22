53763b1bb3122fedb3cea291d8dfaa7ca04f894c
Matthieu Bacconnier <neamar@neamar.fr>
2017-07-22 17:09:29 +0100

    Added translations

--- app/src/main/java/fr/neamar/lolgamedata/tips/WinrateByTimeTip.java
+++ app/src/main/java/fr/neamar/lolgamedata/tips/WinrateByTimeTip.java
@@ -2,10 +2,6 @@ package fr.neamar.lolgamedata.tips;
 
 import fr.neamar.lolgamedata.pojo.Game;
 
-/**
- * Created by neamar on 7/18/2017.
- */
-
 public class WinrateByTimeTip extends Tip {
     public WinrateByTimeTip(Game game) {
         super(game);
