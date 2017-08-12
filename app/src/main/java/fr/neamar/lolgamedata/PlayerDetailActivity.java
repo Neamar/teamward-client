0a3d900da6ea10c1a953aff0a0652c61804742a0
Neamar <neamar@neamar.fr>
2017-08-11 17:19:30 -0700

    Added some common queues

--- app/src/main/java/fr/neamar/lolgamedata/PlayerDetailActivity.java
+++ app/src/main/java/fr/neamar/lolgamedata/PlayerDetailActivity.java
@@ -66,16 +66,18 @@ public class PlayerDetailActivity extends SnackBarActivity {
         queueNames.put("TEAM_BUILDER_DRAFT_RANKED_5x5", R.string.teambuilder_ranked);
 
         // Also with ids
-        queueNames.put("400", R.string.normal);
         queueNames.put("0", R.string.custom);
-        queueNames.put("65", R.string.aram);
-        queueNames.put("8", R.string.normal_3);
+        queueNames.put("2", R.string.blind_pick);
         queueNames.put("4", R.string.ranked_solo_5);
+        queueNames.put("8", R.string.normal_3);
+        queueNames.put("9", R.string.ranked_flex_3);
+        queueNames.put("14", R.string.draft_pick);
+        queueNames.put("65", R.string.aram);
         queueNames.put("318", R.string.arurf);
+        queueNames.put("400", R.string.normal);
+        queueNames.put("410", R.string.teambuilder_ranked);
         queueNames.put("420", R.string.ranked_solo_5);
         queueNames.put("440", R.string.ranked_flex_5);
-        queueNames.put("9", R.string.ranked_flex_3);
-        queueNames.put("410", R.string.teambuilder_ranked);
 
         QUEUE_NAMES = Collections.unmodifiableMap(queueNames);
     }
