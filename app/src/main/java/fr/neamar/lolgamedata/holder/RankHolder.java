dacfb69fd06d2e36e036d5de8eaf75b8bbd5c445
Neamar <neamar@neamar.fr>
2019-01-19 16:07:36 +0000

    Back to op.gg as the stats provider

--- app/src/main/java/fr/neamar/lolgamedata/holder/RankHolder.java
+++ app/src/main/java/fr/neamar/lolgamedata/holder/RankHolder.java
@@ -63,7 +63,7 @@ public class RankHolder extends RecyclerView.ViewHolder implements View.OnClickL
     @Override
     public void onClick(View v) {
         try {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.teamward.xyz/achievements/?summoner=" + URLEncoder.encode(player.summoner.name, "UTF-8") + "&region=" + player.region + "&season=8"));
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://" + player.region + ".op.gg/summoner/userName=" + URLEncoder.encode(player.summoner.name, "UTF-8")));
             v.getContext().startActivity(browserIntent);
 
             Tracker.trackClickOnAchievements((Activity) v.getContext(), player);
