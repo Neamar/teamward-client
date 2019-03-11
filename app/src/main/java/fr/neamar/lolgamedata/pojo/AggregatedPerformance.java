6be6c59ee63a4f64f114b830a6397be640b998fc
Neamar <neamar@neamar.fr>
2019-03-11 20:03:16 +0000

    Fix laner quality script

--- app/src/main/java/fr/neamar/lolgamedata/pojo/AggregatedPerformance.java
+++ app/src/main/java/fr/neamar/lolgamedata/pojo/AggregatedPerformance.java
@@ -18,35 +18,23 @@ public class AggregatedPerformance {
     public double kills;
     public double deaths;
     public double assists;
-    public double csPerMinAvg;
-    public double firstBloodPercent;
-    public double firstTowerPercent;
-    public int doubleKills;
-    public int tripleKills;
-    public int quadraKills;
-    public int pentaKills;
+    private double csPerMinAvg;
+    private double firstBloodPercent;
+    private double firstTowerPercent;
 
     public int gamesCount;
-    public int winsCount;
     public double winsPercent;
-    public int winlaneCount;
-    public double winlanePercent;
+    private double winlanePercent;
 
-    public AggregatedPerformance(JSONObject aggregated) throws JSONException {
+    private AggregatedPerformance(JSONObject aggregated) throws JSONException {
         kills = aggregated.getDouble("kills_average");
         deaths = aggregated.getDouble("deaths_average");
         assists = aggregated.getDouble("assists_average");
         csPerMinAvg = aggregated.getDouble("cs_per_min_average");
         firstBloodPercent = aggregated.getDouble("first_blood_percent");
         firstTowerPercent = aggregated.getDouble("first_tower_percent");
-        doubleKills = aggregated.getInt("double_kills_count");
-        tripleKills = aggregated.getInt("triple_kills_count");
-        quadraKills = aggregated.getInt("quadra_kills_count");
-        pentaKills = aggregated.getInt("penta_kills_count");
         gamesCount = aggregated.getInt("games_count");
-        winsCount = aggregated.getInt("wins_count");
         winsPercent = aggregated.getDouble("wins_percent");
-        winlaneCount = aggregated.getInt("winlane_count");
         winlanePercent = aggregated.getDouble("winlane_percent");
     }
 
@@ -73,10 +61,6 @@ public class AggregatedPerformance {
             qualities.add(context.getString(R.string.often_gets_first_tower));
         }
 
-        if(doubleKills + tripleKills + quadraKills + pentaKills > gamesCount) {
-            qualities.add(context.getString(R.string.multi_kill_machine));
-        }
-
         if(csPerMinAvg > 8.5 && csPerMinAvg < 9.5) {
             qualities.add(context.getString(R.string.good_farmer));
         }
@@ -84,7 +68,7 @@ public class AggregatedPerformance {
             qualities.add(context.getString(R.string.exceptional_farmer));
         }
 
-        if(winlanePercent > 55 && winlanePercent < 65) {
+        if(winlanePercent >= 55 && winlanePercent < 65) {
             qualities.add(context.getString(R.string.good_laner));
         }
         else if(winlanePercent >= 65) {
